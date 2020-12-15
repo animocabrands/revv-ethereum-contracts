961de6ec31b89e6930075b0c5026fb0a4f071c86
Nathan Sala <nathans@animocabrands.com>
2020-12-15 10:17:45 +0000

    Resolve "Update GameeVouchers implementation"

---
+++ contracts/solc-0.6/token/ERC1155721/REVVInventory.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.8;
+
+import "@animoca/ethereum-contracts-assets_inventory/contracts/token/ERC1155721/ERC1155721Inventory.sol";
+import "@animoca/ethereum-contracts-assets_inventory/contracts/token/ERC1155721/IERC1155721BatchTransfer.sol";
+import "@animoca/ethereum-contracts-assets_inventory/contracts/token/ERC1155721/IERC1155721InventoryMintable.sol";
+import "@animoca/ethereum-contracts-assets_inventory/contracts/token/ERC1155721/IERC1155721InventoryBurnable.sol";
+import "@animoca/ethereum-contracts-assets_inventory/contracts/token/ERC1155/IERC1155InventoryCreator.sol";
+import "@animoca/ethereum-contracts-assets_inventory/contracts/metadata/BaseMetadataURI.sol";
+import "@animoca/ethereum-contracts-core_library/contracts/access/MinterRole.sol";
+
+contract REVVInventory is
+    ERC1155721Inventory,
+    IERC1155721BatchTransfer,
+    IERC1155721InventoryMintable,
+    IERC1155721InventoryBurnable,
+    IERC1155InventoryCreator,
+    BaseMetadataURI,
+    MinterRole
+{
+    // solhint-disable-next-line const-name-snakecase
+    string public constant override name = "REVV Inventory";
+    // solhint-disable-next-line const-name-snakecase
+    string public constant override symbol = "REVV-I";
+
+    // ===================================================================================================
+    //                               Admin Public Functions
+    // ===================================================================================================
+
+    /**
+     * Creates a collection.
+     * @dev Reverts if `collectionId` does not represent a collection.
+     * @dev Reverts if `collectionId` has already been created.
+     * @dev Emits a {IERC1155-URI} event.
+     * @dev Emits a {IERC1155Inventory-CollectionCreated} event.
+     * @param collectionId Identifier of the collection.
+     */
+    function createCollection(uint256 collectionId) external onlyOwner {
+        _createCollection(collectionId);
+    }
+
+    /**
+     * @dev See {IERC1155721InventoryMintable-mint(address,uint256)}.
+     */
+    function mint(
+        address to,
+        uint256 nftId
+    ) external override onlyMinter {
+        _mint_ERC721(to, nftId, "", false);
+    }
+
+    /**
+     * @dev See {IERC1155721InventoryMintable-batchMint(address,uint256[])}.
+     */
+    function batchMint(
+        address to,
+        uint256[] calldata nftIds
+    ) external override onlyMinter {
+        _batchMint_ERC721(to, nftIds);
+    }
+
+    /**
+     * @dev See {IERC1155721InventoryMintable-safeMint(address,uint256,bytes)}.
+     */
+    function safeMint(
+        address to,
+        uint256 nftId,
+        bytes calldata data
+    ) external override onlyMinter {
+        _mint_ERC721(to, nftId, data, true);
+    }
+
+    /**
+     * @dev See {IERC1155721InventoryMintable-safeMint(address,uint256,uint256,bytes)}.
+     */
+    function safeMint(
+        address to,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external override onlyMinter {
+        _safeMint(to, id, value, data);
+    }
+
+    /**
+     * @dev See {IERC1155721InventoryMintable-safeBatchMint(address,uint256[],uint256[],bytes)}.
+     */
+    function safeBatchMint(
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    ) external override onlyMinter {
+        _safeBatchMint(to, ids, values, data);
+    }
+
+    // ===================================================================================================
+    //                                 User Public Functions
+    // ===================================================================================================
+
+
+    /**
+     * @dev See {IERC1155InventoryCreator-creator(uint256)}.
+     */
+    function creator(uint256 collectionId) external override view returns(address) {
+        require(!isNFT(collectionId), "Inventory: not a collection");
+        return _creators[collectionId];
+    }
+
+    /**
+     * @dev See {IERC1155721BatchTransfer-batchTransferFrom(address,address,uint256[])}.
+     */
+    function batchTransferFrom(
+        address from,
+        address to,
+        uint256[] calldata nftIds
+    ) external override {
+        _batchTransferFrom_ERC721(from, to, nftIds);
+    }
+
+    /**
+     * @dev See {IERC1155721InventoryBurnable-burnFrom(address,uint256,uint256)}.
+     */
+    function burnFrom(
+        address from,
+        uint256 id,
+        uint256 value
+    ) external override {
+        _burnFrom(from, id, value);
+    }
+
+    /**
+     * @dev See {IERC1155721InventoryBurnable-batchBurnFrom(address,uint256[],uint256[])}.
+     */
+    function batchBurnFrom(
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values
+    ) external override {
+        _batchBurnFrom(from, ids, values);
+    }
+
+    // ===================================================================================================
+    //                                  ERC1155 Internal Functions
+    // ===================================================================================================
+
+    function _uri(uint256 id) internal override(ERC1155InventoryBase, BaseMetadataURI) view returns (string memory) {
+        return BaseMetadataURI._uri(id);
+    }
+}
